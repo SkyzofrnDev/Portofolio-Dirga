3eabdcf655770c7e18a1175b7719151002bebd0b
dg738 <dirgahardekaagustiantara@gmail.com>
2025-05-21 10:55:50 +0700

    Update Button component to link to LinkedIn and fix About component image path

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,19 +3,15 @@ import Bulet from "./Bulet";
 
 const Button = () => {
   return (
-    <div className="flex">
-
-    <div className="w-fit rounded-full px-10 py-5 bg-[#1e1e1e]">
-      <p className="poppins text-white font-bold">LETS SEE</p>
-    </div>
-    <div className="-ml-2">
-
-    <Bulet/>
-    </div>
-
-    </div>
+      <a href="https://www.linkedin.com/in/dirga-hardeka-agustiantara-7421832b8/" className="flex">
+        <div className="w-fit rounded-full px-10 py-5 bg-[#1e1e1e]">
+          <p className="poppins text-white font-bold">LinkedIn</p>
+        </div>
+        <div className="-ml-2">
+          <Bulet />
+        </div>
+      </a>
   );
 };
 
 export default Button;
-
