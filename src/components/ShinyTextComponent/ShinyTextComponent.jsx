ac083d59b81c40aa73a48e59634fa2ada0b013e4
dg738 <dirgahardekaagustiantara@gmail.com>
2025-05-23 08:04:31 +0700

    Update ShinyTextComponent background gradient for improved text visibility during animation.

--- src/components/ShinyTextComponent/ShinyTextComponent.jsx
+++ src/components/ShinyTextComponent/ShinyTextComponent.jsx
@@ -5,7 +5,7 @@ const ShinyText = ({ text, disabled = false, speed = 5, className = '' }) => {
     <div
       className={`text-[#b4b4b4b0] bg-clip-text inline-block ${disabled ? '' : 'animate-shine'} ${className}`}
       style={{
-        backgroundImage: 'linear-gradient(120deg, rgba(255, 255, 255, 0) 40%, rgba(255, 255, 255, 0.8) 50%, rgba(255, 255, 255, 0) 60%)',
+        backgroundImage: 'linear-gradient(120deg, rgba(255, 255, 255, 0) 40%, rgba(255, 255, 255, 1) 50%, rgba(255, 255, 255, 0) 60%)',
         backgroundSize: '200% 100%',
         WebkitBackgroundClip: 'text',
         animationDuration: animationDuration,
