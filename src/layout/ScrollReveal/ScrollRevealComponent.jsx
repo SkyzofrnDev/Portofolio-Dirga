e0aeeafe8a5a87507c0fa0d1a4188855227e296a
dg738 <dirgahardekaagustiantara@gmail.com>
2025-05-23 07:15:06 +0700

    Refactor component exports in index files to include ScrollReveal component and ensure TextSlider is correctly exported. Remove unused Text file from TextReveal directory.

---
+++ src/layout/ScrollReveal/ScrollRevealComponent.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ScrollReveal from "./ScrollRevealComponent";
+
+const ScrollRevealComponent = () => {
+  return (
+    <ScrollReveal
+      baseOpacity={0}
+      enableBlur={true}
+      baseRotation={5}
+      blurStrength={10}
+    >
+      When does a man die? When he is hit by a bullet? No! When he suffers a
+      disease? No! When he ate a soup made out of a poisonous mushroom? No! A
+      man dies when he is forgotten!
+    </ScrollReveal>
+  );
+};
+
+export default ScrollRevealComponent;
