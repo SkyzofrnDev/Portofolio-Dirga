c04a31c7a974582103ac1043c30fa8ff450974ad
SkyzofrnDev <dirgahardekaagustiantara@gmail.com>
2025-06-10 15:22:16 +0700

    Update Project

--- src/layout/ProjectDesign/ProjectDesign.jsx
+++ src/layout/ProjectDesign/ProjectDesign.jsx
@@ -6,14 +6,14 @@ const ProjectDesign = () => {
     <section className="bg-[#0b0b0d] px-5 pb-24 lg:px-24 text-white">
       <div className=" lg:flex justify-between items-end ">
         <div className="uppercase">
-          <p className="font-semibold text-3xl">My Design Work</p>
-          <p className="font-bold text-5xl xl:text-6xl mt-5 lg:mt-10">
+          <p className="font-semibold text-sm md:text-3xl">My Design Work</p>
+          <p className="font-bold text-4xl md:text-5xl xl:text-6xl md:mt-5 lg:mt-10">
             My <span className="text-[#1299F5]">Expertise</span>{" "}
             <p>at a Glance</p>
           </p>
         </div>
         <div className="">
-          <p className="text-xl mt-5 lg:mt-0 ">
+          <p className="text-sm md:text-xl mt-2 lg:mt-0 ">
             Explore the skills that define my professional{" "}
             <p>journey and drive my accomplishments.</p>
           </p>
